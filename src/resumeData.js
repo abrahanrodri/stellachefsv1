9cedcef9ec7cf48f480d741b05497784bd12e3a5
Abe <abrahan.rodri@gmail.com>
2019-10-18 11:56:48 -0400

    adding wireframe

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://abrahanrodri.github.io/",
+    "name": "Stella Chefs",
+    "role": "sensory chef",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "logo": "/./public/images/logo.jpg",
+    "roleDescription": " A splendid culinary experience with your chef Sean McConnon.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-instagram"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I cook blah blah",
+    "address":"NYC",
+    "website":"stellachefs.com",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"dish 1",
+        "description":"armenian",
+        "imgurl":"../public/images/portfolio/armenian.jpg"
+      },
+      {
+        "name":"dish 2",
+        "description":"cuban",
+        "imgurl":"../public/images/portfolio/cuban.jpg"
+      },
+      {
+        "name":"dish 3",
+        "description":"indian",  
+        "imgurl":"../public/images/portfolio/indian.jpg"
+      },
+      {
+        "name":"dish 4",
+        "description":"israeli",
+        "imgurl":"../public/images/portfolio/israeli.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"big baller company"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"corporate big checks company"
+      }
+    ]
+  }
+  
+  export default resumeData
